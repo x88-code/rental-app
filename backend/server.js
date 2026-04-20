d9cbefd0e5541488481814a3378ce8a79bec7488
x88-code <xgh6695@gmail.com>
2026-04-20 20:34:47 +0300

    Removed missing ensureDevUser module

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,12 @@ dotenv.config();
 
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
-const ensureDevUser = require("./src/utils/ensureDevUser");
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
     await connectDB();
-    await ensureDevUser();
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
